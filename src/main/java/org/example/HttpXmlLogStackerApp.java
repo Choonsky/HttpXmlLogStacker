29a1d757324bf4a4a41e2035ca14dd0fa5efc22c
Stanislav Nemirovsky <choonsky@gmail.com>
2023-01-18 17:08:09 +0600

    Initial commit

---
+++ src/main/java/org/example/HttpXmlLogStackerApp.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class HttpXmlLogStackerApp {
+    private static final int EXECUTION_DELAY_MS = 5000;
+
+    public static void main(String[] args) throws InterruptedException {
+        while(true) {
+
+            // stack log files
+
+            Thread.sleep(EXECUTION_DELAY_MS);
+
+        }
+    }
+}
